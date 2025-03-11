f424fb864e8fe316dfa9cb5cc24715bdfe3f8277
Snayff <snaith2@gmail.com>
2025-03-11 14:28:31 +0000

    completed purchase and sale functionality

--- person.gd
+++ person.gd
@@ -23,19 +23,25 @@ extends Node
 
 
 #region VARS
+var f_name: String = ""
+var is_alive: bool = true
 var health: int = 3
+var job: String = ""
+## goods held
+var stockpile: Dictionary = {}
+var goods_considered_sellable: Array = ["grain", "water"]
+## goods to try and buy
+var desired_goods: Array = ["grain", "water"]
 var required_grain: int = 1
 var required_water: int = 2
-var job: String = ""
-var stockpile: Dictionary = {}
-var is_alive: bool = true
 var no_grain_damage: int = 1
 var no_water_damage: int = 1
 #endregion
 
 
 #region FUNCS
-func _init(job_: String, starting_goods: Dictionary) -> void:
+func _init(f_name_: String, job_: String, starting_goods: Dictionary) -> void:
+	f_name = f_name_
 	job = job_
 
 	# add values from starting goods
@@ -46,10 +52,24 @@ func produce() -> void:
 	match job:
 		"farmer":
 			stockpile["grain"] += 10
+			print(str(
+				f_name, 
+				" produced 10 grain."
+			))
 
 		"water collector":
 			stockpile["water"] += 20
-
+			print(str(
+				f_name, 
+				" produced 20 water."
+			))
+			
+		"gold miner":
+			stockpile["money"] += 5
+			print(str(
+				f_name, 
+				" produced 🪙5.",
+			))
 		_:
 			pass
 
@@ -76,14 +96,28 @@ func consume() -> void:
 func get_goods_for_sale() -> Dictionary:
 	var goods_to_sell: Dictionary = {}
 	# determine all goods above threshold
-	for good in stockpile:
+	for good in goods_considered_sellable:
 		if stockpile[good] > Constants.NEED_TO_BUY_FLOOR:
 			goods_to_sell[good] = stockpile[good] - Constants.NEED_TO_BUY_FLOOR
 
 	return goods_to_sell
 
-
-
+func get_goods_to_buy() -> Dictionary:
+	var goods_to_buy: Dictionary = {}
+	
+	for good in desired_goods:
+		# if we have 0, buy up to the buy-floor
+		if good not in stockpile:
+			goods_to_buy[good] = Constants.NEED_TO_BUY_FLOOR
+		
+		else:
+			var amount_to_buy = max(0, Constants.NEED_TO_BUY_FLOOR - stockpile[good])
+			if amount_to_buy != 0:
+				goods_to_buy[good] = amount_to_buy
+		
+	return goods_to_buy
+			
+	
 
 
 
--- sim.gd
+++ sim.gd
@@ -24,6 +24,11 @@ extends Node
 
 #region VARS
 var people: Array[Person]
+var good_prices: Dictionary = {
+	"grain": 10,
+	"water": 10,
+	"wood": 20,
+}
 
 #endregion
 
@@ -36,9 +41,15 @@ func _ready() -> void:
 
 func _create_people() -> void:
 	var num_people: int = 3
+	var names: Array = [
+		"sally", 
+		"reginald",
+		"estaban"
+	]
 	var job_allocation: Dictionary = {
 		"farmer": 1,
 		"water collector": 1,
+		"gold miner": 1,
 	}
 	var starting_goods: Dictionary = {
 		"money": 100,
@@ -56,12 +67,17 @@ func _create_people() -> void:
 
 	# assign job and starting goods
 	for i in range(num_people):
-		people.append(Person.new(jobs[i], starting_goods))
+		people.append(Person.new(names[i], jobs[i], starting_goods))
 
 func resolve_turn() -> void:
 	var saleable_goods: Dictionary = {}  # { good: { person: { amount: 123, money_made: 123 } } }
 	var desired_goods: Dictionary = {}  # { good: { person: { amount: 123 } } }
 	
+	# logging stuff
+	var supply: Dictionary = {}  # {name: { good: amount } }
+	var demand: Dictionary = {}  # {name: { good: amount } }
+	
+	print(">>> Production & Consumption Phase")
 	# simulate each person's turn
 	for person in people:
 		if !person.is_alive:
@@ -70,7 +86,7 @@ func resolve_turn() -> void:
 		# produce and consume
 		person.produce()
 		person.consume()
-
+		
 		# get goods would sell, based on what is left
 		var goods_to_sell: Dictionary = person.get_goods_for_sale()
 		for good in goods_to_sell:
@@ -83,18 +99,126 @@ func resolve_turn() -> void:
 				"amount": goods_to_sell[good],
 				"money_made": 0
 			}
+			
+			
+			# update supply for logging
+			if person.f_name not in supply:
+				supply[person.f_name] = {}
+			supply[person.f_name][good] = goods_to_sell[good]
+			
 
-		# get goods would buy
-		#var goods_to_buy: Dictionary = person.get_goods_to_buy()
-		# TODO: add to desired_goods
+		# get goods people want to buy
+		var goods_to_buy: Dictionary = person.get_goods_to_buy()
+		for good in goods_to_buy:
+			if good not in desired_goods:
+				desired_goods[good] = {}
+				
+			desired_goods[good][person] = goods_to_buy[good]
+			
+			# update demand for logging
+			if person.f_name not in demand:
+				demand[person.f_name] = {}
+			demand[person.f_name][good] = goods_to_buy[good]
+			
+	print(">>> Market Setup")
+	print(str(
+		"Supply: ",
+		supply,
+		"\nDemand", 
+		demand
+	))
 
+	# conduct sale
+	print(">>> Market Opens")
+	var purchasers: Array = []
+	var amount_to_buy: int = 0
+	var sellers: Array = []
+	var saleable_remaining: int = 0
+	var log_message: String = ""
+	
+	for good in saleable_goods:
+		# no buyers, move on
+		if good not in desired_goods:
+			continue
+		
+		# get sellers and randomise order
+		sellers = saleable_goods[good].keys()
+		sellers.shuffle()
+		
+		# loop sellers of the good, pick a buyer at random and let them buy
+		# all that they want
+		for seller in sellers:
+			var amount_to_sell: int = saleable_goods[good][seller]["amount"]
+			
+			# people who want to buy the good in question
+			purchasers = desired_goods[good].keys()
+			purchasers.shuffle()
+			
+			for buyer in purchasers:
+				log_message = ""
+				
+				# make purchase
+				amount_to_buy = min(floor(buyer.stockpile["money"] / good_prices[good]), desired_goods[good][buyer])
+				var cost = good_prices[good] * amount_to_buy
+				buyer.stockpile["money"] -= cost
+				seller.stockpile["money"] += cost
+				
+				# add to buyers stockpile
+				if good not in buyer.stockpile:
+					buyer.stockpile[good] = 0
+				buyer.stockpile[good] += amount_to_buy
+				seller.stockpile[good] -= amount_to_buy
+				
+				# update desired good and purchasers
+				desired_goods[good][buyer] -= amount_to_buy
+				if desired_goods[good][buyer] <= 0:
+					desired_goods[good].erase(buyer)
+				
+				# update remaining
+				var goods_left: int = max(0, amount_to_sell - amount_to_buy)
+				saleable_goods[good][seller]["amount"] = goods_left
+				saleable_goods[good][seller]["money_made"] += cost
+				
+				log_message = str(
+					seller.f_name,
+					" sold ",
+					amount_to_buy, 
+					" ",
+					good,
+					" to ",
+					buyer.f_name,
+					" for 🪙", 
+					cost, 
+					" (",
+					good_prices[good], 
+					" each).",
+				)
+				print(log_message)
+				
+				# seller has stock remaining, find new buyer
+				if goods_left > 0:
+					continue
+				
+				# sellers stock is empty, find new seller
+				elif goods_left == 0:
+					break
+					
+		
+		
+	print(">>> Market Closes")
+	var results: Dictionary = {}
+	# var saleable_goods: Dictionary = {}  # { good: { person: { amount: 123, money_made: 123 } } }
+	for good in saleable_goods.keys():
+		for seller in saleable_goods[good]:
+			if seller.f_name not in results:
+				results[seller.f_name] = 0
+			results[seller.f_name] += saleable_goods[good][seller]["money_made"]
+	print(str(
+		"Sales:",
+		results
+	))
 
-	# TODO: loop desired_goods,
-	#		look for desired_good in saleable_goods, remove desired # until saleable = 0
-
-
-
-
+	print(">>> Turn Resolved >>>>>>>>>>>>>>>>>>>>")
 
 
 
